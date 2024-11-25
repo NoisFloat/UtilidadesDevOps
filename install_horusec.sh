d824960c301f942dfd98b28855ad4e612c706784
NoisFloat <144391743+NoisFloat@users.noreply.github.com>
2024-11-24 21:30:16 -0600

    I wanna be a DevSecOps Engineer

---
+++ install_horusec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This is for installation "Horusec" in an image of docker "ubuntu:latest"
+
+#Instalation of dependencies
+apt-get update -y
+apt install curl -y
+apt install jq -y
+apt install git -y
+#apt install sudo 
+
+if [ "$(which curl)" != "" ]
+then
+echo "Tienes instalado curl"
+else
+echo "Ocurrio un error en la instalacion de dependencias"
+fi
