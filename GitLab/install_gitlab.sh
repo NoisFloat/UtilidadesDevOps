fa13c8e5b967427ed726cb5e44d41d9955fa88c2
Noel Guevara <144391743+NoisFloat@users.noreply.github.com>
2024-11-26 17:03:37 -0600

    Update install_gitlab.sh

--- GitLab/install_gitlab.sh
+++ GitLab/install_gitlab.sh
@@ -1,1 +1,6 @@
-
+apt-get update -y
+apt-get install -y curl openssh-server ca-certificates tzdata perl
+apt-get install -y postfix
+apt-get install -y curl
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="https://gitlab.nois.com" apt-get install gitlab-ee
